1d1edaac6343c23e40d1178849842a87f08d0b94
Syed Ziaul Habib <roobon@gmail.com>
2025-10-22 09:23:06 +0600

    first commit

---
+++ test/index.php
@@ -0,0 +1,26 @@
+
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to PHP</h1>
+    <?php 
+    
+    print "<h1>Hellow World!</h1>";
+    
+    ?>
+   
+php_check_syntax
+
+   
+</body>
+</html>
+
+<?php ?>
+
+
